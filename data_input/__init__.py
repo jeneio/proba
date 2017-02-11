0e2d4ad89c1c51e684c91eeaa51a4db9679ebaae
Jakab Gábor <jkbgbr@gmail.com>
2017-02-11 18:16:32 +0100

    első válasz

--- data_input/__init__.py
+++ data_input/__init__.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+
 #sziasztok, Osszián vagyok, üdv, hali
 #hello
 #de
 #proba
 #na most elvileg sikerült
+
+
+# beraktam egy első sort. Ez legyen majd minden python fájlod első sora, hogy ne legyen gond az ékezetes betűkkel!
+

15af13c9c3384bd0d1f672fd50c4a04071a3a896
Oszi <jeneiosszian@gmail.com>
2017-02-12 18:13:27 +0100

    gyakorlás tutorial alapján tovább. moduloknál nem igazán vagyok képben mi fog kelleni nekem

--- data_input/__init__.py
+++ data_input/__init__.py
@@ -8,3 +8,16 @@ print('Hi '+'my name is Osszian')
 print(5+8)
 print(int('8')+5)
 print(float('8.5')+5)
+condition = 1
+while condition < 10:
+    print (condition)
+    condition += 1
+x = 4
+y = 5
+z = 10
+if x > y:
+    print ('x>y')
+elif x > z:
+    print ('x>z')
+else:
+    print ('x a legkisebb')

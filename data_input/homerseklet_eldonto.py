de1cc310b09b367babc1a570e1e5c95719ef7695
Oszi <jeneiosszian@gmail.com>
2017-03-11 20:24:04 +0100

    ez fogja eldönteni, hogy a keresztmetszetben azonos e a hőmérséklet, ezt bővíthetem majd nem csak egyenlő hőmérsékletre

---
+++ data_input/homerseklet_eldonto.py
@@ -0,0 +1,13 @@
+# listát létrehozni
+
+elsokm = [22.5,22.4,22.1,22.9]
+
+#megadni, hogy mekkora hőmérsékleteltérést tekintünk azonos hőmérsékletnek
+y = input ('mekkora hőmérsékletkülönbséget tekintsek azonos hőmérsékletnek? ')
+x = max(elsokm)-min(elsokm)
+#eldönteni, hogy azonos-e a hőmérséklet a vizsgált km 4 pontján
+if x < y:
+    print('a vizsgált km minden pontja egyenlő hőmérsékletűnek tekinthető')
+    print('a max. hőm. kül. = ',x)
+else: print('a vizsgált km pontjai nem egyenlő hőmérsékletűek','a max. hőm. kül. = ',x)
+

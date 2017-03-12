c81340a522158ec054c4c5a946bf7647a3014281
Jakab Gábor <jkbgbr@gmail.com>
2017-03-12 21:06:27 +0100

    hibak javitva

--- data_input/homerseklet_eldonto.py
+++ data_input/homerseklet_eldonto.py
@@ -1,14 +1,15 @@
 # listát létrehozni
 
-elsokm = [22.5,22.4,22.1,22.9]
+elsokm = [22.5, 22.4, 22.1, 22.9]
 
-#megadni, hogy mekkora hőmérsékleteltérést tekintünk azonos hőmérsékletnek
-y = input ('mekkora hőmérsékletkülönbséget tekintsek azonos hőmérsékletnek? ')
-y=int(y)
-x = max(elsokm)-min(elsokm)
-#eldönteni, hogy azonos-e a hőmérséklet a vizsgált km 4 pontján
-if x < y:
+# megadni, hogy mekkora hőmérsékleteltérést tekintünk azonos hőmérsékletnek
+y = input('mekkora hőmérsékletkülönbséget tekintsek azonos hőmérsékletnek? ')
+y = int(y)
+x = abs(max(elsokm)-min(elsokm))  # ABS!!!
+# eldönteni, hogy azonos-e a hőmérséklet a vizsgált km 4 pontján
+if x <= y:
     print('a vizsgált km minden pontja egyenlő hőmérsékletűnek tekinthető')
-    print('a max. hőm. kül. = ',x)
-else: print('a vizsgált km pontjai nem egyenlő hőmérsékletűek','a max. hőm. kül. = ',x)
+    print('a max. hőm. kül. = ', x)
+else:
+    print('a vizsgált km pontjai nem egyenlő hőmérsékletűek','a max. hőm. kül. = ', x)
 

69468fbbebe958016f0ab4ada138ad1efd40e369
Oszi <jeneiosszian@gmail.com>
2017-03-18 21:05:52 +0100

    nem sikerül a lista elemeit számmá konvertálnom a stackoverflow és a google használata után se (legalábbis szerintem ez a baja amiért nem számol átlagot)

--- data_input/atlagszamito.py
+++ data_input/atlagszamito.py
@@ -1,4 +1,6 @@
 import csv
+import math
+
 # listát létrehozni meglévő fájlból
 
 # fájlnév változatlan rész
@@ -13,10 +15,27 @@ ds = [list(x) for x in zip(*d)]  # transzponálás;http://stackoverflow.com/ques
 #átlag előállítása
 import statistics as st
 #átlag megadása
-x = input ('Mekkora átlagot vegyek? (db adat) ')
-x = int(x)
-for i in range (0,(x-1)):
-    y = (sum(ds[i]))/x
+#x = input ('Mekkora átlagot vegyek? (db adat) ')
+#x = int(x)
+#for i in range (0,(x-1)):
+#    y = (sum(ds[i]))/x
+#print('átlag=',y)
+#a listában szereplő allisták elemeinek számmá konvertálása
+for y in ds:
+    for z in y:
+        float()
+#ellenőrzés, hogy szám-e?
+add = ds[0][0] + ds[0][1]
+print(add)
 
-print('átlag=',y)
+#valamiért még mindig nem jó.. és szerintem ez a probémája amiért az átlagszámítás se fut.
 
+#átlag számítása - Jakab Gábor
+ATLAGOLT_HOSSZ = 2
+
+#l = [1, 2, 3, 4, 5, 6]
+_ret = (zip(*[iter(ds)]*ATLAGOLT_HOSSZ))
+
+# a kapott listabol:
+atl = [sum(x) / ATLAGOLT_HOSSZ for x in _ret]
+print(atl)

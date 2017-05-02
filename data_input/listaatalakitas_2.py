18a38a4183a0a4c0fd565001224174f24249e072
Oszi <jeneiosszian@gmail.com>
2017-05-02 19:15:50 +0200

    elmozdulások előállítva a pozíciókból. az egyes szenzorok hőmérséklet változására előállítva a nyúlások.

--- data_input/listaatalakitas_2.py
+++ data_input/listaatalakitas_2.py
@@ -2,15 +2,15 @@
 
 import csv
 import itertools
-from data_input import PATH_HO
+from __init__ import PATH_HO, PATH_SARU, PATH_NYUL, ACEL, SARU_TAV, HO_KM
 import pickle
+from collections import Counter
 
 
 # ezzel csinálom meg az adatok rendezését
 def darabolo(be, nagydb, kisdb, kezdet):
-    """eznegy fuggveny"""
     lista = []
-    z = kezdet * 4
+    z = kezdet
     while z < len(be):
         l2 = []
         for x in range(0, kisdb):
@@ -148,7 +148,11 @@ def valami_beolvaso(path):
         # a bolvasott fájl rendezése km, időpont és szenzor szerint
         keresztmetszetlista = []
         for keresztmetszet in range(0, 21):
-            km = darabolo(d_km, 85, 4, keresztmetszet)
+            # meghívom a darabolót az egy sorban levő adataimra.
+            # 84 érték tartozik egy időponthoz (+egy üres).
+            # 4 szenzorunk van.
+            # az utolsó paraméter a darabolás kezdetének pozícióját határozza meg
+            km = darabolo(d_km, 85, 4, 4*keresztmetszet)
             # list_1 = [km[0], km[1], km[2]]
             keresztmetszetlista.append(km)
         # keresztmetszetlista [keresztmetszet] [időpont(4érték)] [szenzorérték]
@@ -160,6 +164,77 @@ def valami_beolvaso(path):
             pickle.dump(egypercesatlag, f)
 
     return egypercesatlag
+
+# ezzel csinálom meg az adatok rendezését és bővítését, hogy minden szenzorhoz legyen
+def sarudarabolo(be, nagydb, kisdb, kezdet):
+    lista = []
+    z = kezdet
+    while z < len(be):
+        l2 = []
+        for x in range(0, kisdb):
+            l2.append(float(be[z + x]))
+        z = z + nagydb
+        lista.append(l2)
+    return lista
+
+
+def saru_beolvaso(path):
+
+    # mérési adatok beolvasása
+    with open('.'.join([path, 'ret'])) as f:
+        reader = csv.reader(f, delimiter="\t")
+        d = list(reader)
+    # elmozdulások előállítása minden nap 0. időponthoz viszonyítva
+    if '_saru_' in path:
+        d = [sor[0:24] for sor in d]
+        _ret = []
+        for sor in d:
+            _ret.append([float(x)-float(y) for x, y in zip(sor, d[0])])
+        d = _ret
+
+    d_saru = list(itertools.chain.from_iterable(d))
+    saru = []
+    # meghívom a darabolót az egy sorban levő adataimra.
+    # 24 érték tartozik egy időponthoz (+egy üres).
+    # 2 sarunk van.
+    # az utolsó paraméter a darabolás kezdetének pozícióját határozza meg
+    for pilon in range(0,12):
+        saru.append(darabolo(d_saru, 24, 2, 2*pilon))
+
+    return saru
+
+# előállítom minden pillanathoz minden szenzorra a delta T hőmérséklet különbséget a 0. időponthoz viszonyítva.
+def homersekletkulonbseg(lista):
+        lista = [sor[0:84] for sor in lista]
+        _ret = []
+        for sor in lista:
+            _ret.append([float(x)-float(y) for x, y in zip(sor, lista[0])])
+        return _ret
+
+# nyúlások kiszámítása 0. időponthoz és fáljba írása és listába rakása a későbbi forgalom és hőmérséklet elmozdulások
+# szétválasztásához
+def homelmozdulas (homlista,anyagallando,sarutav,path):
+    nyullista = []
+    with open(path + '.ret', 'w') as f:
+        for x in range (0,len(homlista)):
+                string1 = ""
+                kisnyullista = []
+                for y in range (0, len(homlista[0])):
+                    a = int(y / 4)
+                    if HO_KM[a]:
+                        # nyúlások előállítása, listába rakása és kiíratás előkészítése
+                        nyul = anyagallando*sarutav[a]*homlista[x][y]
+                        string = str(nyul) + " "
+                        string1 = string1 + string
+                        kisnyullista.append(nyul)
+                f.write(string1 + "\n")
+                nyullista.append(kisnyullista)
+    return nyullista
+
+# a forgalom és a hőmérséklet elmozdulásainak szétválasztására 1 perces módusz értékeket állítok elő mozgó átlaggal.
+# a kapott módusz értékek várhatóan csak a hőmérsékleti hatásokat tartalmazzák majd
+def modusz (bemenolista,idotartam):
+    
 
 
 def napvizsgalo(path):
@@ -230,6 +305,66 @@ def run():
             string = ",".join(map(str, eredmeny)) + "\n"
             f.write(string)
 
+def run2():
+    for x in range (1,2):
+        if x < 10:
+            file_nev = "0" + str(x)
+        else:
+            file_nev = str(x)
+        path = PATH_SARU + file_nev
+        # path = 'C:\\Users\\szatm\\PycharmProjects\\oszi\\M0_ho_201309' + str(x)
+        eredmeny = saru_beolvaso(path)
+        print (eredmeny[0][0])
+
+        # kiíROM FÁJLBA AZ ELMOZDUÁS ÉRTÉKEKET
+        with open('201309'+file_nev+'_elm.ret', 'w') as f:
+            for z in range(0,len(eredmeny[0])):
+                string1 = ""
+                for y in range(0, len(eredmeny)):
+                    string = " ".join(map(str, eredmeny[y][z])) + " "
+                    string1 = string1 + string
+                f.write(string1 +"\n")
+
+# hőmérsékletkülönbségek
+def run_deltaT():
+    for x in range (1,31):
+        if x < 10:
+            file_nev = "0" + str(x)
+        else:
+            file_nev = str(x)
+        path = PATH_HO + file_nev
+        # path = 'C:\\Users\\szatm\\PycharmProjects\\oszi\\M0_ho_201309' + str(x)
+        # mérési adatok beolvasása
+        with open('.'.join([path, 'ret'])) as f:
+            reader = csv.reader(f, delimiter="\t")
+            d = list(reader)
+        eredmeny = homersekletkulonbseg(d)
+        nyul_path = PATH_NYUL + file_nev
+
+        a=homelmozdulas(eredmeny,ACEL,SARU_TAV,nyul_path)
+
+
+# ez itt egy próbálkozás, félbehagyva egyelőre
+def saruatlag():
+    x = 1
+    if x < 10:
+        file_nev = "0" + str(x)
+    else:
+        file_nev = str(x)
+    path = PATH_SARU + file_nev
+    # path = 'C:\\Users\\szatm\\PycharmProjects\\oszi\\M0_ho_201309' + str(x)
+    eredmeny = saru_beolvaso(path)
+    s_atlagt = dbcsokkento(20,eredmeny)
+    s_atlag=transzponalonagy(s_atlagt)
+    print(len(s_atlag[0]))
+    with open('saru_atlag.ret', 'w') as f:
+        for x in range (0,len(s_atlag[4])):
+        #for y in range (0,len(s_atlag[4][0])):
+        #string = " ".join(map(str, s_atlag[4][x]))+" "
+            for y in range(0,20):
+                string = " ".join(map(str, s_atlag[4][x])) + "\n"
+                f.write(string)
+
 if __name__ == '__main__':
 
-    run()
+    run2()
